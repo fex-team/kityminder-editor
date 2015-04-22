e979e6d88d58465df56ea142ea21f1b0927dd10a
zhangbobell <zhangbobell@163.com>
2015-04-22 18:33:29 +0800

    增加了 priority Directive

---
+++ ui/directive/priorityEditor/priorityEditor.directive.js
@@ -0,0 +1,31 @@
+angular.module('kmEditorUI')
+
+    .directive('priorityEditor', ['commandBinder', function(commandBinder) {
+        return {
+            restrict: 'E',
+            templateUrl: 'ui/directive/priorityEditor/priorityEditor.html',
+            scope: {
+                minder: '='
+            },
+            link: function($scope) {
+                var minder = $scope.minder;
+                var priorities = [];
+
+                for (var i = 0; i < 10; i++) {
+                    priorities.push(i);
+                }
+
+	            commandBinder.bind(minder, 'priority', $scope);
+
+	            $scope.priorities = priorities;
+
+	            $scope.getPriorityTitle = function(p) {
+		            switch(p) {
+			            case 0: return '移除优先级';
+			            default: return '优先级' + p;
+		            }
+	            }
+            }
+
+        }
+    }]);
