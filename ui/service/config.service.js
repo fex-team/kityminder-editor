c8f5ee108e781dcf57359fb73dc8d79105aa8181
张博 <zhangbo21@baidu.com>
2015-07-01 18:53:26 +0800

    增加了缩放比例的配置

--- ui/service/config.service.js
+++ ui/service/config.service.js
@@ -3,10 +3,21 @@ angular.module('kityminderEditor')
 
 		return {
 			_default: {
+
+                // 右侧面板最小宽度
 				ctrlPanelMin: 250,
+
+                // 右侧面板宽度
 				ctrlPanelWidth: parseInt(window.localStorage.__dev_minder_ctrlPanelWidth) || 250,
-				dividerWidth: 3,
-				defaultLang: 'zh-cn'
+
+				// 分割线宽度
+                dividerWidth: 3,
+
+                // 默认语言
+				defaultLang: 'zh-cn',
+
+                // 放大缩小比例
+                zoom: [10, 20, 30, 50, 80, 100, 120, 150, 200]
 			},
 			getConfig: function(key) {
 				return key == undefined ? this._default : (this._default[key] || null);
