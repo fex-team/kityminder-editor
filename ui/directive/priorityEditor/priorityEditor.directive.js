a4e1133e8deb669a397eb52a7a1b97fd878b8d46
张博 <zhangbo21@baidu.com>
2015-07-14 20:21:42 +0800

    add priority to topbar

--- ui/directive/priorityEditor/priorityEditor.directive.js
+++ ui/directive/priorityEditor/priorityEditor.directive.js
@@ -7,6 +7,7 @@ angular.module('kityminderEditor')
             scope: {
                 minder: '='
             },
+            replace: true,
             link: function($scope) {
                 var minder = $scope.minder;
                 var priorities = [];
