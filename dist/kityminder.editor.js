465ae3f9bd63642329c6ae92ce26d3897af40f6b
张博 <zhangbo21@baidu.com>
2015-08-05 14:36:22 +0800

    modified the memory service

--- dist/kityminder.editor.js
+++ dist/kityminder.editor.js
@@ -1,6 +1,6 @@
 /*!
  * ====================================================
- * kityminder-editor - v1.0.21 - 2015-08-04
+ * kityminder-editor - v1.0.21 - 2015-08-05
  * https://github.com/fex-team/kityminder-editor
  * GitHub: https://github.com/fex-team/kityminder-editor 
  * Copyright (c) 2015 ; Licensed 
@@ -1859,19 +1859,54 @@ angular.module('kityminderEditor')
  */
 angular.module('kityminderEditor')
     .service('memory', function() {
-    return {
-        get: function(item) {
-            var ls = window.localStorage;
-            var memory = ls.uiMemory ? JSON.parse(ls.uiMemory) : {};
-            return null || memory[item];
-        },
 
-        set: function(item, value) {
-            var ls = window.localStorage;
-            var memory = ls.uiMemory ? JSON.parse(ls.uiMemory) : {};
-            memory[item] = value;
-            ls.uiMemory = JSON.stringify(memory);
+        function isQuotaExceeded(e) {
+            var quotaExceeded = false;
+            if (e) {
+                if (e.code) {
+                    switch (e.code) {
+                        case 22:
+                            quotaExceeded = true;
+                            break;
+                        case 1014:
+                            // Firefox
+                            if (e.name === 'NS_ERROR_DOM_QUOTA_REACHED') {
+                                quotaExceeded = true;
+                            }
+                            break;
+                    }
+                } else if (e.number === -2147024882) {
+                    // Internet Explorer 8
+                    quotaExceeded = true;
+                }
+            }
+            return quotaExceeded;
         }
+
+        return {
+            get: function(key) {
+                var value = window.localStorage.getItem(key);
+                return null || JSON.parse(value);
+            },
+
+            set: function(key, value) {
+                try {
+                    window.localStorage.setItem(key, JSON.stringify(value));
+                    return true;
+                } catch(e) {
+                    if (isQuotaExceeded(e)) {
+                        return false;
+                    }
+                }
+            },
+            remove: function(key) {
+                var value = window.localStorage.getItem(key);
+                window.localStorage.removeItem(key);
+                return value;
+            },
+            clear: function() {
+                window.localStorage.clear();
+            }
     }
 });
 angular.module('kityminderEditor')
