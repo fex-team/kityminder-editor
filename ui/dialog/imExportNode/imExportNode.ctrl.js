1742d2e8f565089511fd9a91da8511c4e2213c7a
张博 <zhangbo21@baidu.com>
2015-09-23 02:43:32 +0800

    优化了 textarea 的 tab 事件

--- ui/dialog/imExportNode/imExportNode.ctrl.js
+++ ui/dialog/imExportNode/imExportNode.ctrl.js
@@ -29,17 +29,65 @@ angular.module('kityminderEditor')
         $scope.shortCut = function(e) {
             e.stopPropagation();
 
-            if (e.keyCode == 13 && e.shiftKey == false) {
-                $scope.ok();
-            }
+            //if (e.keyCode == 13 && e.shiftKey == false) {
+            //    $scope.ok();
+            //}
 
             if (e.keyCode == 27) {
                 $scope.cancel();
             }
 
             // tab 键屏蔽默认事件 和 backspace 键屏蔽默认事件
-            if (e.keyCode == 9 || e.keyCode == 8) {
+            if (e.keyCode == 8 && type == 'export') {
                 e.preventDefault();
+            }
+
+            if (e.keyCode == 9) {
+                e.preventDefault();
+                var $textarea = e.target;
+                var pos = getCursortPosition($textarea);
+                var str = $textarea.value;
+                $textarea.value = str.substr(0, pos) + '\t' + str.substr(pos);
+                setCaretPosition($textarea, pos + 1);
+            }
+
+        };
+
+        /*
+        * 获取 textarea 的光标位置
+        * @Author: Naixor
+        * @date: 2015.09.23
+        * */
+        function getCursortPosition (ctrl) {
+            var CaretPos = 0;	// IE Support
+            if (document.selection) {
+                ctrl.focus ();
+                var Sel = document.selection.createRange ();
+                Sel.moveStart ('character', -ctrl.value.length);
+                CaretPos = Sel.text.length;
+            }
+            // Firefox support
+            else if (ctrl.selectionStart || ctrl.selectionStart == '0') {
+                CaretPos = ctrl.selectionStart;
+            }
+            return (CaretPos);
+        }
+
+        /*
+         * 设置 textarea 的光标位置
+         * @Author: Naixor
+         * @date: 2015.09.23
+         * */
+        function setCaretPosition(ctrl, pos){
+            if(ctrl.setSelectionRange) {
+                ctrl.focus();
+                ctrl.setSelectionRange(pos,pos);
+            } else if (ctrl.createTextRange) {
+                var range = ctrl.createTextRange();
+                range.collapse(true);
+                range.moveEnd('character', pos);
+                range.moveStart('character', pos);
+                range.select();
             }
         }
 
