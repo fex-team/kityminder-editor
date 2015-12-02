ad3e8180a5cee0b469f8309a72e31dcfe5cc6cf8
hy <hongyesoftware@gmail.com>
2015-12-02 18:03:47 +0800

    修复快捷键抬起顺序限制的BUG；增加在Input模式选中文字进行cmd/ctrl + b/i的处理；将新增节点后就进入input模式变成不直接进入input模式

--- src/runtime/node.js
+++ src/runtime/node.js
@@ -32,7 +32,7 @@ define(function(require, exports, module) {
                         minder.execCommand(command, '分支主题');
 
                         // provide in input runtime
-                        runtime.editText();
+                        // runtime.editText();
                     } else {
                         minder.execCommand(command);
                         fsm.jump('normal', 'command-executed');
