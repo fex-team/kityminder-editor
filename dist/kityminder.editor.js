13efd4cb98846cd556d7d63e60de8857fed90e5d
马金花儿 <o.o@mug.dog>
2018-03-27 17:25:34 +0800

    feat(release): release v1.0.63

--- dist/kityminder.editor.js
+++ dist/kityminder.editor.js
@@ -1,6 +1,6 @@
 /*!
  * ====================================================
- * kityminder-editor - v1.0.62 - 2018-03-23
+ * kityminder-editor - v1.0.63 - 2018-03-27
  * https://github.com/fex-team/kityminder-editor
  * GitHub: https://github.com/fex-team/kityminder-editor 
  * Copyright (c) 2018 ; Licensed 
